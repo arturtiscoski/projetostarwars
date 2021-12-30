4d17c0c13ab9632ac98576077d32ab4a192d59da
arturtiscoski <artur@consystec.com.br>
2021-12-30 11:31:06 -0300

    Updates

--- backend/server/services/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const SkyWarsApi = require('../skyWarsApi/SkyWarsApi');
-
-const router = new express.Router();
-
-router.route('/skywars/:id?')
-  .get(SkyWarsApi.get);
-
-module.exports = router;
