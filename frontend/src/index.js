48230810291cd2b1d81fb15f9d4e57b9dc04bc6c
arturtiscoski <artur@consystec.com.br>
2021-12-29 22:04:29 -0300

    Adiciona gh-pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import Footer from './footer/Footer';
+import ghpages from 'gh-pages';
+const path = require('path');
+
+ghpages.publish(path.resolve(__dirname, '../backend/dist'), function(err) {console.log(err)});
 
 ReactDOM.render(
   <App />,
